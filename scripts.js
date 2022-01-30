54884ff62fe97b426667778ee68e5ae16ac42461
jfrey212 <eojyerf@gmail.com>
2022-01-29 18:32:04 -0600

    Added container div. Added JS to create a row div. Added css styling for container, row, column divs

---
+++ scripts.js
@@ -0,0 +1,7 @@
+let container = document.querySelector(".container");
+// create a row div
+
+let row = document.createElement('div');
+row.setAttribute('class', 'row');
+row.textContent = "A new div";
+container.appendChild(row);
