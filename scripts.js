02c3fdd1ea10417aed88abf3a58270c4a1d07ec4
jfrey212 <eojyerf@gmail.com>
2022-01-29 22:27:05 -0600

    Created create grid function and a loop to create specified number of rows

--- scripts.js
+++ scripts.js
@@ -1,7 +1,15 @@
 let container = document.querySelector(".container");
 // create a row div
 
-let row = document.createElement('div');
-row.setAttribute('class', 'row');
-row.textContent = "A new div";
-container.appendChild(row);
+
+function createGrid(numberOfRows) {
+    for (let i = 0; i < numberOfRows; i++) {
+      let row = document.createElement('div');
+
+      row.setAttribute('class', 'row');
+      row.textContent = "A new div";
+      container.appendChild(row);
+    }
+}
+
+createGrid(3);
